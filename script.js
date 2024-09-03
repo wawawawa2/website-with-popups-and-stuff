b2b85a2275ff8f8e45c51e5c2779cdeda525ce2e
wawawawa2 <utbnorsor@skola.malmo.se>
2024-09-03 09:45:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function openPopup() {
-    window.open('popup.html', 'popupWindow', 'width=600, height=400')
+    window.open('popup', 'popupWindow', 'width=600, height=400')
 }
 function openPopup2() {
-  window.open('popup2.html', 'popupWindow2', 'width=400,height=200')
+  window.open('popup2', 'popupWindow2', 'width=400,height=200')
 }
 
 alert('This is the main window.')
