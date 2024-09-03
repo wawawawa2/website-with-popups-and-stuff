d0f39953b5821a91a64672bc0bccb5f5ae61fb60
wawawawa2 <utbnorsor@skola.malmo.se>
2024-09-03 09:28:39 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function openPopup() {
+    window.open('popup.html', 'popupWindow', 'width=600, height=400')
+}
+function openPopup2() {
+  window.open('popup2.html', 'popupWindow2', 'width=400,height=200')
+}
+
+alert('This is the main window.')
